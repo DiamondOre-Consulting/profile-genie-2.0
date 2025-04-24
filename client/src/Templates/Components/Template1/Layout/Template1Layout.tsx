7ee692fd22d0cd98e87d95485042974c9a5e4978
Akash Kumar Singh <itsakash18.06@gmail.com>
2025-04-24 15:24:05 +0530

    Fixed some issues

--- client/src/Templates/Components/Template1/Layout/Template1Layout.tsx
+++ client/src/Templates/Components/Template1/Layout/Template1Layout.tsx
@@ -28,7 +28,7 @@ const Template1Layout = ({ children, fullName, aboutActive, contactActive, produ
                     </Link>
                     <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
                         {/* <button type="button" className="text-white bg-[#F43F5E] hover:bg-[#F43F5E]/90 cursor-pointer focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2 text-center "><IconShare3 /></button> */}
-                        <button onClick={() => setIsOpen(!isOpen)} type="button" className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded={isOpen}>
+                        <button onClick={() => setIsOpen(!isOpen)} type="button" className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded={isOpen}>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -36,7 +36,7 @@ const Template1Layout = ({ children, fullName, aboutActive, contactActive, produ
                         </button>
                     </div>
                     <div className={`items-center justify-between  w-full md:flex md:w-auto md:order-1 transition duration-300 relative z-[100] ease-in-out ${isOpen ? 'fixed top-0 left-0 right-0 bottom-0 flex-col' : 'hidden'}`} id="navbar-cta">
-                        <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg cursor-pointer md:p-0 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="flex flex-col items-center justify-center bg-[#101828] p-4 mt-4 font-medium border border-gray-100 rounded-lg cursor-pointer md:p-0 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                             <Link to="home" smooth={true} duration={500} className="hover:underline">Home</Link>
                             {aboutActive && <Link to="about" smooth={true} duration={500} offset={-100} className="hover:underline">About</Link>}
                             {serviceActive && <Link to="services" smooth={true} duration={500} offset={-80} className="hover:underline">Services</Link>}
