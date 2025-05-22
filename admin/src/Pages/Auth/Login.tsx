2861beb4508192f92c72213691490a59c5b55a39
Akash Kumar Singh <itsakash18.06@gmail.com>
2025-05-22 16:47:53 +0530

    Updated

--- admin/src/Pages/Auth/Login.tsx
+++ admin/src/Pages/Auth/Login.tsx
@@ -34,7 +34,7 @@ const Login = () => {
 
     const handleGoogleLogin = async () => {
         try {
-            window.location.href = "https://server.profilegenie.in/api/v1/auth/google";
+            window.location.href = "https://profile-genie-2-0-server.onrender.com/api/v1/auth/google";
             // window.location.href = "http://localhost:8000/api/v1/auth/google";
         } catch (err) {
             console.log(err);
