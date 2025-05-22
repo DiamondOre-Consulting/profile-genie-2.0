43f81b094a89ab77af7b0aad8aad08a6bf5d64d3
Akash Kumar Singh <itsakash18.06@gmail.com>
2025-05-22 16:41:44 +0530

    Updated

--- admin/src/Helper/axiosInstance.ts
+++ admin/src/Helper/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { toast } from 'sonner'
 
-const BASE_URL = "http://localhost:8000/api/v1"
+// const BASE_URL = "http://localhost:8000/api/v1"
+const BASE_URL = "https://profile-genie-2-0-server.onrender.com/"
 // const BASE_URL = "https://server.profilegenie.in/api/v1"
 
 const axiosInstance = axios.create()
