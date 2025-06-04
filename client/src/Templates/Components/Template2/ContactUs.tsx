6bc33d3a62c15688b8d14b97c0d5dd02b85cdf79
Akash Kumar Singh <itsakash18.06@gmail.com>
2025-06-04 21:51:09 +0530

    Updated few functionality

--- client/src/Templates/Components/Template2/ContactUs.tsx
+++ client/src/Templates/Components/Template2/ContactUs.tsx
@@ -3,10 +3,10 @@ import { Contact, Mail, MapPin, Phone } from "lucide-react";
 import { useState } from "react";
 import { FaDownload } from "react-icons/fa";
 import PhoneInput from "react-phone-input-2";
-import 'react-phone-input-2/lib/style.css'
+import "react-phone-input-2/lib/style.css";
 
 const ContactUs = ({ portfolioData }: { portfolioData: portfolioResponse }) => {
-  const [whatsAppNumber, setWhatsAppNumber] = useState<number | null>(null)
+  const [whatsAppNumber, setWhatsAppNumber] = useState<number | null>(null);
 
   const portfolioUrl = window.location.href;
   const message = `Hello, find my portfolio here:\n\n${portfolioUrl}\n\n📌 Click the link above to view my portfolio!`;
@@ -32,90 +32,96 @@ const ContactUs = ({ portfolioData }: { portfolioData: portfolioResponse }) => {
         <div className="mt-10 mb-10">
           <dl className="grid grid-cols-1 gap-10 md:grid-cols-3">
             {/* Address Section */}
-              {portfolioData?.contactData?.address && portfolioData.contactData.address.length > 0 && (
-            <div className="flex items-start">
-              <div className="flex-shrink-0">
-                <div className="flex items-center justify-center w-12 h-12 text-white bg-[#0891B2] rounded-lg">
-                  <MapPin className="w-6 h-6" />
-                </div>
-              </div>
-                <div className="ml-4">
-                  <dt className="text-lg font-medium leading-6 text-gray-900">
-                    Addresses
-                  </dt>
-                  <dd className="mt-2 space-y-2">
-                    {portfolioData.contactData.address.map((addr, index) => (
-                      <div key={index} className="text-gray-600">
-                        {addr.title && (
-                          <div className="font-medium text-gray-900">
-                            {addr.title}
-                          </div>
-                        )}
-                        <div className="text-base">
-                          {addr.detail}
+            {portfolioData?.contactData?.address &&
+              portfolioData.contactData.address.length > 0 && (
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center w-12 h-12 text-white bg-[#0891B2] rounded-lg">
+                      <MapPin className="w-6 h-6" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <dt className="text-lg font-medium leading-6 text-gray-900">
+                      Addresses
+                    </dt>
+                    <dd className="mt-2 space-y-2">
+                      {portfolioData.contactData.address.map((addr, index) => (
+                        <div key={index} className="text-gray-600">
+                          {addr.title && (
+                            <div className="font-medium text-gray-900">
+                              {addr.title}
+                            </div>
+                          )}
+                          <div className="text-base">{addr.detail}</div>
                         </div>
-                      </div>
-                    ))}
-                  </dd>
+                      ))}
+                    </dd>
+                  </div>
                 </div>
-            </div>
               )}
 
             {/* Phone Number Section */}
-              {portfolioData?.contactData?.phoneList && portfolioData.contactData.phoneList[0].phone && (
-            <div className="flex items-start">
-              <div className="flex-shrink-0">
-                <div className="flex items-center justify-center w-12 h-12 text-white bg-[#0891B2] rounded-lg">
-                  <Phone className="w-6 h-6" />
+            {portfolioData?.contactData?.phoneList &&
+              portfolioData.contactData.phoneList[0].phone && (
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center w-12 h-12 text-white bg-[#0891B2] rounded-lg">
+                      <Phone className="w-6 h-6" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <dt className="text-lg font-medium leading-6 text-gray-900">
+                      Phone Numbers
+                    </dt>
+                    <dd className="mt-1">
+                      {portfolioData.contactData.phoneList.map(
+                        (phoneItem, index) => (
+                          <div key={index}>
+                            <a
+                              href={`tel:+91${phoneItem?.phone}`}
+                              className="text-base text-gray-600 hover:text-[#0891B2] transition-colors"
+                            >
+                              <span className="pb-1 text-lg">+</span>
+                              {phoneItem?.phone}
+                            </a>
+                          </div>
+                        )
+                      )}
+                    </dd>
+                  </div>
                 </div>
-              </div>
-                <div className="ml-4">
-                  <dt className="text-lg font-medium leading-6 text-gray-900">
-                    Phone Numbers
-                  </dt>
-                  <dd className="mt-1">
-                    {portfolioData.contactData.phoneList.map((phoneItem, index) => (
-                      <div key={index}>
-                        <a
-                          href={`tel:+91${phoneItem.phone}`}
-                          className="text-base text-gray-600 hover:text-[#0891B2] transition-colors"
-                        >
-                          <span className="pb-1 text-lg">+</span>{phoneItem.phone}
-                        </a>
-                      </div>
-                    ))}
-                  </dd>
-                </div>
-            </div>
               )}
 
             {/* Email Section */}
-              {portfolioData?.contactData?.emailList &&portfolioData?.contactData?.emailList[0].email && (
-            <div className="flex items-start">
-              <div className="flex-shrink-0">
-                <div className="flex items-center justify-center w-12 h-12 text-white bg-[#0891B2] rounded-lg">
-                  <Mail className="w-6 h-6" />
+            {portfolioData?.contactData?.emailList &&
+              portfolioData?.contactData?.emailList[0].email && (
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center w-12 h-12 text-white bg-[#0891B2] rounded-lg">
+                      <Mail className="w-6 h-6" />
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <dt className="text-lg font-medium leading-6 text-gray-900">
+                      Email
+                    </dt>
+                    <dd className="mt-2">
+                      {portfolioData?.contactData?.emailList.map(
+                        (emailItem, index) => (
+                          <div key={index}>
+                            <a
+                              target="_blank"
+                              href={`mailto:"${emailItem?.email}"`}
+                              className="text-base text-gray-600 hover:text-[#0891B2] transition-colors"
+                            >
+                              {emailItem?.email}
+                            </a>
+                          </div>
+                        )
+                      )}
+                    </dd>
+                  </div>
                 </div>
-              </div>
-                <div className="ml-4">
-                  <dt className="text-lg font-medium leading-6 text-gray-900">
-                    Email
-                  </dt>
-                  <dd className="mt-2">
-                  {portfolioData?.contactData?.emailList.map((emailItem, index) => (
-                      <div key={index}>
-                        <a
-                        target="_blank"
-                          href={`mailto:"${emailItem?.email}"`}
-                          className="text-base text-gray-600 hover:text-[#0891B2] transition-colors"
-                        >
-                          {emailItem?.email}
-                        </a>
-                      </div>
-                    ))}
-                  </dd>
-                </div>
-            </div>
               )}
           </dl>
         </div>
@@ -126,11 +132,11 @@ const ContactUs = ({ portfolioData }: { portfolioData: portfolioResponse }) => {
             </div>
             <div className="flex gap-2 max-auto items-center justify-center max-w-[35rem] w-[95vw]">
               <PhoneInput
-                country={'in'}
+                country={"in"}
                 buttonStyle={{
                   border: "1px solid #000",
                   backgroundColor: "#fff",
-                  color: "#000"
+                  color: "#000",
                 }}
                 inputStyle={{
                   width: "100%",
@@ -141,15 +147,17 @@ const ContactUs = ({ portfolioData }: { portfolioData: portfolioResponse }) => {
                   height: "40px",
                   borderRadius: "4px",
                   backgroundColor: "#fff",
-                  color: "#000"
+                  color: "#000",
                 }}
                 dropdownStyle={{
                   backgroundColor: "#fff",
                   color: "#000",
-                  border: "1px solid #000"
+                  border: "1px solid #000",
                 }}
                 value={whatsAppNumber?.toString()}
-                onChange={phone => setWhatsAppNumber(phone ? Number(phone) : null)}
+                onChange={(phone) =>
+                  setWhatsAppNumber(phone ? Number(phone) : null)
+                }
               />
               <a
                 target="_blank"
@@ -165,22 +173,31 @@ const ContactUs = ({ portfolioData }: { portfolioData: portfolioResponse }) => {
         <div className="flex items-center sm:flex-row flex-col justify-between max-w-[35rem] mx-auto gap-2 lg:text-center md:justify-center">
           {portfolioData?.contactData?.contactCSV && (
             <button
-              onClick={() => window.open(portfolioData?.contactData?.contactCSV, "_blank")}
+              onClick={() =>
+                window.open(portfolioData?.contactData?.contactCSV, "_blank")
+              }
               className="relative w-full h-12 flex items-center mx-auto justify-center p-2 px-3 sm:px-10 text-[0.9rem] overflow-hidden font-semibold text-white bg-[#0891B2] border-none rounded-full cursor-pointer group"
             >
-              <div className='flex items-center justify-center gap-1 sm:gap-4'>
-                <Contact className='text-[1.4rem]' /> Save Contact
+              <div className="flex items-center justify-center gap-1 sm:gap-4">
+                <Contact className="text-[1.4rem]" /> Save Contact
               </div>
             </button>
           )}
 
           {portfolioData?.contactData?.brochureLink?.link && (
             <button
-              onClick={() => window.open(portfolioData?.contactData?.brochureLink?.link, "_blank")}
+              onClick={() =>
+                window.open(
+                  portfolioData?.contactData?.brochureLink?.link,
+                  "_blank"
+                )
+              }
               className="relative w-full h-12 p-2 px-3 sm:px-10 text-[0.9rem] overflow-hidden font-semibold text-white bg-[#0891B2] border-none rounded-full cursor-pointer group"
             >
-              <div className='flex items-center justify-center gap-2 sm:gap-4'>
-                <FaDownload className='text-[1.1rem]' /> {portfolioData?.contactData?.brochureLink?.tagline || "Download Brochure"}
+              <div className="flex items-center justify-center gap-2 sm:gap-4">
+                <FaDownload className="text-[1.1rem]" />{" "}
+                {portfolioData?.contactData?.brochureLink?.tagline ||
+                  "Download Brochure"}
               </div>
             </button>
           )}
