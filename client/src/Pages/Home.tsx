c227ef9f25d60b35a4983660e455357a302930fd
Akash Kumar Singh <itsakash18.06@gmail.com>
2025-07-11 14:07:18 +0530

    Added robotics

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -12,9 +12,9 @@ const Home = () => {
     <div className="bg-[#151515] w-full overflow-x-hidden">
       <HomeLayout>
         <Hero />
-        <div className="fixed z-[2000] bottom-0 right-4">
+        <div className="fixed z-[2000]  bottom-0 right-4   overflow-hidden">
           <iframe
-            src="https://my.spline.design/greetingrobot-QpnzkZv9sC2rdtzgWVHN7Aim/"
+            src="https://my.spline.design/greetingrobot-wPb20PaGBuFW6MLjsEHmqAMC/"
             width="165%"
             height="150%"
           ></iframe>
