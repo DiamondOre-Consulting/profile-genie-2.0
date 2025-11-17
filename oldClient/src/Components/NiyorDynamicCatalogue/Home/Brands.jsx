7b956b6f8863a326659c8bfab8f753865a263ac4
Zoya774 <zoya.rasonline@gmail.com>
2025-11-17 12:08:14 +0530

    update changes

--- oldClient/src/Components/NiyorDynamicCatalogue/Home/Brands.jsx
+++ oldClient/src/Components/NiyorDynamicCatalogue/Home/Brands.jsx
@@ -17,11 +17,14 @@ import img13 from '../../../assets/Niyor/brands/brand14.png';
 import img14 from '../../../assets/Niyor/brands/brand15.png'
 import img15 from '../../../assets/Niyor/brands/JLR.png'
 import img16 from '../../../assets/Niyor/brands/hdfc.png'
+import img17 from '../../../assets/Niyor/brands/sc.png'
+
 
 
 const Brands = () => {
   // Array of brand logos (you can add more)
-  const brandLogos = [taj ,img1 , img2 , img3 , img4 ,img5 ,img6  , img7 ,img8 , img9 , img10 , img11 , img12 , img13 , img14 , img15 , img16];
+  const brandLogos = [taj ,img1 , img2 , img3 , img4 ,img5 ,img6  , img7 ,img8 , img9 , img10 , img11 , img12 , img13 , img14 ];
+  const b2 =[img15 , img16 , img17]
 
   return (
     <div
@@ -41,6 +44,30 @@ const Brands = () => {
         {/* Grid for brand logos */}
         <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-x-8 gap-y-10 w-full max-w-6xl px-4">
           {brandLogos.map((logo, index) => (
+            <div
+              key={index}
+              className="flex justify-center items-center hover:scale-105 transition-transform duration-300"
+            >
+              <img
+                src={logo}
+                alt={`Brand ${index}`}
+                className="object-contain h-16 md:h-28"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+
+        <div className="relative flex flex-col items-center justify-start py-10 h-full text-center space-y-12">
+        {/* Heading */}
+        <p className="text-[#D4B274] text-3xl md:text-5xl font-bold px-8 head">
+ Brands we have worked with 
+        </p>
+
+        {/* Grid for brand logos */}
+        <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-x-8 gap-y-10 w-full max-w-6xl px-4">
+          {b2.map((logo, index) => (
             <div
               key={index}
               className="flex justify-center items-center hover:scale-105 transition-transform duration-300"
